22546be87f588cdb77356eace0a2cfb512e2295c
thomas <thomas@paulin.co.nz>
2017-09-26 21:27:03 +1300

    Create files with URLs for match parsing

---
+++ src/snc/scraper/boxscore.py
@@ -0,0 +1,2 @@
+# For scraping http://www.aucklandsnchockey.com/leagues/hockey_boxscores.cfm?clientID=5788&leagueID=23341&gameID=[some-id-here]
+# That is, it is for scraping the match information
---
+++ src/snc/scraper/schedule.py
@@ -0,0 +1,4 @@
+# For parsing http://www.aucklandsnchockey.com/leagues/schedules.cfm?clientid=5788&leagueid=23341
+# This page is used in addition to the printable schedule because the printable schedule does not
+#   have the boxscore links but for everything else the printable schedule is preferred because
+#   it has the whole season on one page so fewer requests are made
