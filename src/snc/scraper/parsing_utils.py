8caa154c2ef80e1d868975abda780741b1858179
thomas <thomas@paulin.co.nz>
2017-09-27 23:28:15 +1300

    Create a util file

---
+++ src/snc/scraper/parsing_utils.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from pytz import timezone, utc
+
+
+def capitalise(string):
+    words = string.strip().lower().split(' ')
+    upper_words = []
+    for word in words:
+        print(word[0:1].upper() + word[1:])
+        upper_words.append(word[0:1].upper() + word[1:])
+    return ' '.join(upper_words)
+
+def parse_date(date_str, str_format, timezone):
+    tz = timezone("Pacific/Auckland")
+    return tz.localize(datetime.strptime(date_str, str_format));
