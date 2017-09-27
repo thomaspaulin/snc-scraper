462b7e4f5b5ec0acc18bfd84d0d5e5a23a41f258
thomas <thomas@paulin.co.nz>
2017-09-27 18:38:35 +1300

    Add disclaimer about selectedYear differing from season

--- src/snc/scraper/schedule.py
+++ src/snc/scraper/schedule.py
@@ -5,11 +5,17 @@ from bs4 import BeautifulSoup
 #   have the boxscore links but for everything else the printable schedule is preferred because
 #   it has the whole season on one page so fewer requests are made
 
-# TODO this is mutually exclusive with today_scores.py
-
 #TODO ADD selectedMonth=3&selectedYear=2017 to get the month of interest page
 
 def parse_boxscore_urls(soup):
+    """Returns the boxscore URLs for all those matches with them
+
+    Note: To use a selectedYear that is not the current season's year you must
+          change season otherwise no games will be shown
+
+          e.g.,
+          You cannot be in the 2017 season and use selectedYear=2016
+    """
     links = soup.select('table.boxscores')[4].select('a')
     boxscore_anchor_elems = [x for x in links if 'hockey_boxscores' in x['href']]
     return [elem['href'] for elem in boxscore_anchor_elems]
