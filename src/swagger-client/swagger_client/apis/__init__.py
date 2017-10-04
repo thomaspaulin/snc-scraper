1e9eaa5d593f0d556a9f96abdacdc99534411b07
thomas <thomas@paulin.co.nz>
2017-10-04 20:20:39 +1300

    Host swagger client on GitHub

--- src/swagger-client/swagger_client/apis/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-
-# import apis into api package
-from .goalies_api import GoaliesApi
-from .goals_api import GoalsApi
-from .matches_api import MatchesApi
-from .penalties_api import PenaltiesApi
-from .players_api import PlayersApi
-from .teams_api import TeamsApi
